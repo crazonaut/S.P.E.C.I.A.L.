c2a9ed3a3dbaa20e3b9bc1da369970e271fa5cfd
crazonaut <iogata21@punahou.edu>
2016-07-06 13:01:46 -1000

    stuff
    test

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ var highscore = 300;
 
 var press = function() {
 	uscore++;
-	document.getElementById('score').innerHTML = "Best Points NA: " + uscore;
+	document.getElementById('score').innerHTML = "Points: " + uscore;
 
 	if (uscore>highscore) {
-		document.getElementById('hiscore').innerHTML = "New Highscore (GET A LIFE): " + uscore;
+		document.getElementById('hiscore').innerHTML = "You have reached 300 points";
 		return;
 
 	}
