b22a953df8f8f10c6a22d86b974b06624bfdd089
el-ROSSO <rosshagino@icloud.com>
2016-07-08 13:38:10 -1000

    af
    dafasdfsa

---
+++ js/Perception.js
@@ -0,0 +1,9 @@
+document.getElementById('input_box').addEventListener('keyup',function (event){
+	var string =  document.getElementById("input_box").value
+
+	if string ===''{
+		document.getElementById('link').href = google.com
+	}
+});
+
+// ᖗ◔ᴥ◔ᖘʕ◔ᴥ◔ʔ
