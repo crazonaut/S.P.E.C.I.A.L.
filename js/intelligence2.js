e155278eb5d3e321255cca682c89b8fc3de8c48b
Hudson673 <hsakai@icloud.com>
2016-07-08 18:17:45 -1000

    making first question
    what is the python thing again?

---
+++ js/intelligence2.js
@@ -0,0 +1,15 @@
+function press(){
+	console.log("test");
+	window.location.replace('/intelligence3.html');
+};
+
+function press(){
+	console.log("test");
+	window.location.replace('/intelligence3.html');
+};
+
+function press(){
+	console.log("test");
+	window.location.replace('/intelligence3.html');
+};
+
