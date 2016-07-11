8d92231ea8732dbc940f5f73091d35ff1e0892d1
Hudson673 <hudsakai@icloud.com>
2016-07-11 13:37:56 -1000

    ;op4qwjrt
    wept2

--- js/intelligence2.js
+++ js/intelligence2.js
@@ -1,5 +1,13 @@
 function press(){
 	console.log("test");
-	window.location.replace('/intelligence3.html');
+	window.location.replace('/intelligence.html');
 };
 
+function press1() {
+	console.log("log");
+	window.location.replace('/intelligence.html');
+};
+function presscorrect() {
+	console.log("correct");
+	window.location.replace('/intelligence3.html');
+};
