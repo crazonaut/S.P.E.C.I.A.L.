32bc7da3cc131811271a86048802378b386344bb
Hudson673 <hudsakai@icloud.com>
2016-07-11 13:53:44 -1000

    ougASEF
    ijkdiqvw

---
+++ js/intelligence3.js
@@ -0,0 +1,13 @@
+function press(){
+	console.log("test");
+	window.location.replace('/intelligence.html');
+};
+
+function press2() {
+	console.log("log");
+	window.location.replace('/luck.html');
+};
+function presscorrect2() {
+	console.log("correct");
+	window.location.replace('/intelligence3.html');
+};
