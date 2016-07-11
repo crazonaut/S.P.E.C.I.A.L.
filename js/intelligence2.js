978f6702e24d6b843d23683481cb8aa2d99f4870
Hudson673 <hudsakai@icloud.com>
2016-07-11 12:54:32 -1000

    gfegfdbtgfbvcdfv
     vbcb vc

--- js/intelligence2.js
+++ js/intelligence2.js
@@ -3,13 +3,3 @@ function press(){
 	window.location.replace('/intelligence3.html');
 };
 
-function press(){
-	console.log("test");
-	window.location.replace('/intelligence3.html');
-};
-
-function press(){
-	console.log("test");
-	window.location.replace('/intelligence3.html');
-};
-
