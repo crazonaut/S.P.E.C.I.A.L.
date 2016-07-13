c4debcff00075b899fa0840eb047f3a544b7377a
crazonaut <iogata21@punahou.edu>
2016-07-13 13:10:36 -1000

    a
    a

--- js/Strength.js
+++ js/Strength.js
@@ -7,7 +7,7 @@ function press() {
 }
 
 function check(){
- if (uscore < 1){
+ if (uscore < 300){
   document.getElementById('hiscore').innerHTML = "YOU FAILED! Press command+r to try again, or command+q if you give up :3";
   uscore = 0;
   clearInterval(myVar);
