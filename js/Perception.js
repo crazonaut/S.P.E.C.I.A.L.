513f541acf23bd06e9acd50ce4e36f8039926992
el-ROSSO <rosshagino@icloud.com>
2016-07-13 13:27:09 -1000

    hgjl
    lijk

--- js/Perception.js
+++ js/Perception.js
@@ -1,7 +1,7 @@
 document.getElementById('input_box').addEventListener('keyup',function (event){
 	var string =  document.getElementById("input_box").value
 
-	if string ===''{
+	if (input_box ==='HELLO'){
 		document.getElementById('link').href = google.com
 	}
 });
